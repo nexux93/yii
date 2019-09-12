22317a95bae3779ca2d3ec6b68ae359dce173d37
n.ronzin <nexux93@yandex.ru>
2019-09-12 14:23:57 +0300

    first commit

---
+++ models/Active.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace app\models;
+
+
+use yii\base\Model;
+
+class Active extends Model
+{
+public $title;
+public $startDay;
+public $endDay;
+public $userId;
+public $description;
+public $repeat = false;
+public $blocked = false;
+}
