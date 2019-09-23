0c3b5b34b74783af76959e222982ded70a0c11fe
n.ronzin <nexux93@yandex.ru>
2019-09-23 10:13:25 +0300

    добавлено 2 миграции для бд

--- controllers/ActiveController.php
+++ controllers/ActiveController.php
@@ -7,22 +7,51 @@
 
 namespace app\controllers;
 
-
 use app\models\Active;
 use yii\base\Controller;
+use yii\helpers\VarDumper;
 
 class ActiveController extends Controller
 {
-public function actionIndex() {
-    \Yii::$app->seo->setTitle('mordor');
-    return 'ok';
-}
-public function actionView() {
-    $activeItem = new Active();
-    $activeItem->title = 'ABabinili';
+    public function actionIndex()
+    {
+        return 'ok';
+    }
 
-    return $this->render('view.twig', [
-        'model' => $activeItem
-    ]);
-}
+    public function actionView()
+    {
+        $activeItem = new Active();
+        $activeItem->title = 'ABabinili';
+
+        return $this->render('view.twig', [
+            'model' => $activeItem
+        ]);
+    }
+
+    public function actionCreate()
+    {
+        $activeItem = new Active();
+        $activeItem->title = 'Create Quest';
+
+        return $this->render('create.twig', [
+            'model' => $activeItem
+        ]);
+    }
+
+    public function actionDelite()
+    {
+        return 'Action@Delite';
+    }
+    public function actionSubmit()
+    {
+        $model = new Active();
+        if ($model->load(\Yii::$app->request->post())) {
+            if ($model->validate()) {
+                return 'Success' . VarDumper::export($model->attributes);
+            } else {
+                return "Failed" . VarDumper::export($model ->errors);
+            }
+        }
+        return 'Active@Submit';
+    }
 }
---
+++ migrations/m190923_070805_active.php
@@ -0,0 +1,51 @@
+<?php
+
+use yii\db\Migration;
+use yii\db\Schema;
+/**
+ * Class m190923_070805_active
+ */
+class m190923_070805_active extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+$this->createTable('active', [
+    'activeId' => Schema::TYPE_SMALLINT,
+    'title' => Schema::TYPE_CHAR,
+    'startDay'=> Schema::TYPE_DATE,
+    'endDay'=> Schema::TYPE_DATE,
+    'userId'=> Schema::TYPE_SMALLINT,
+    'description'=> Schema::TYPE_CHAR,
+    'email'=> Schema::TYPE_CHAR,
+    'repeat'=> Schema::TYPE_BOOLEAN,
+    'blocked'=> Schema::TYPE_BOOLEAN
+]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('active');
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m190923_070805_active cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
